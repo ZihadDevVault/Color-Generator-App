6767673db5b1c1ec0255f80ef1324bbc8f3a1aab
ZihadDevVault <engr.zihadmia@gmail.com>
2025-04-12 22:55:16 +0600

    Color Copy Section is Done

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
-let RgbGeneratorButton=document.getElementById('bgChangebtn');
-let HexGeneratorButton=document.getElementById('hexbgChangebtn')
-let ShowColorRgbCode=document.getElementById('backgroundColor');
-let ShowColorHexCode=document.getElementById('hexbackgroundColor');
-let DocBody=document.getElementsByTagName('body')[0]
+let RgbGeneratorButton = document.getElementById('bgChangebtn');
+let rgbCopyButton = document.getElementById('bgColorCopy')
+let HexGeneratorButton = document.getElementById('hexbgChangebtn')
+let hexCopyButton = document.getElementById('hexColorCopy')
+let ShowColorRgbCode = document.getElementById('backgroundColor');
+let ShowColorHexCode = document.getElementById('hexbackgroundColor');
+let DocBody = document.getElementsByTagName('body')[0]
 
+// ColorGenerator Section 
 
+function GenerateColor() {
+    let Red = Math.floor(Math.random() * 255 + 1)
+    let Green = Math.floor(Math.random() * 255 + 1)
+    let Blue = Math.floor(Math.random() * 255 + 1)
 
-RgbGeneratorButton.addEventListener('click',function(){
-    let RgbColor=GenerateRGBCode()
-    ShowColorRgbCode.value=RgbColor;
-        DocBody.style.backgroundColor=RgbColor;
-})
-
-HexGeneratorButton.addEventListener('click',function(){
-    let HexColor=GenerateHexCode()
-    ShowColorHexCode.value=HexColor;
-        DocBody.style.backgroundColor=HexColor;
-})
-
-function GenerateRGBCode(){
-    let Red=Math.floor(Math.random()*255+1)
-    let Green=Math.floor(Math.random()*255+1)
-    let Blue=Math.floor(Math.random()*255+1)
-
-    return `rgb(${Red},${Green},${Blue})`
-}
-function GenerateHexCode(){
-    let Red=Math.floor(Math.random()*255+1)
-    let Green=Math.floor(Math.random()*255+1)
-    let Blue=Math.floor(Math.random()*255+1)
-   
-
-    return `#${Red.toString(16)}${Green.toString(16)}${Blue.toString(16)}`
+    return{
+        RGb:`rgb(${Red},${Green},${Blue})`,
+        Hex:`#${Red.toString(16)}${Green.toString(16)}${Blue.toString(16)}`
+    }
+    
 }
 
+// Color Change Section 
+function ColorChange(CButton) {
+    CButton.addEventListener('click', function () {
+        let Color = GenerateColor()
+        ShowColorRgbCode.value = Color.RGb;
+        ShowColorHexCode.value = Color.Hex;
+        DocBody.style.backgroundColor = Color.Hex;
+    })
+}
 
+// Color Copy Section 
+function CopyColor(Button, FromData) {
+    Button.addEventListener('click', function () {
+        navigator.clipboard.writeText(FromData.value)
+    })
+}
+
+// Change Color Function Call 
+
+ColorChange(RgbGeneratorButton)
+ColorChange(HexGeneratorButton)
+
+// Copy Color Function Call 
+CopyColor(rgbCopyButton, ShowColorRgbCode)
+CopyColor(hexCopyButton, ShowColorHexCode)
