8bf996113f5672d528a26cfb748b070efcec4b32
ZihadDevVault <engr.zihadmia@gmail.com>
2025-04-16 13:48:42 +0600

    Random color Generator Apps is completed

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let RgbGeneratorButton = document.getElementById('bgChangebtn');
-let rgbCopyButton = document.getElementById('bgColorCopy')
-let HexGeneratorButton = document.getElementById('hexbgChangebtn')
-let hexCopyButton = document.getElementById('hexColorCopy')
-let ShowColorRgbCode = document.getElementById('backgroundColor');
-let ShowColorHexCode = document.getElementById('hexbackgroundColor');
+let DocWithId=(id)=>{
+    return document.getElementById(id)
+}
+
+let RgbGeneratorButton = DocWithId('bgChangebtn')
+let rgbCopyButton =  DocWithId('bgColorCopy')
+let HexGeneratorButton = DocWithId('hexbgChangebtn')
+let hexCopyButton = DocWithId('hexColorCopy')
+let CloseButton=DocWithId('closeBtn')
+let ShowColorRgbCode = DocWithId('backgroundColor');
+let ShowColorHexCode = DocWithId('hexbackgroundColor')
 let Toast=document.getElementsByClassName('toast')[0];
-let ToastMsg=document.getElementById('toastMsg');
+let ToastMsg=DocWithId('toastMsg')
 let DocBody = document.getElementsByTagName('body')[0];
 
 // ColorGenerator Section 
@@ -35,15 +40,31 @@ function ColorChange(CButton) {
 // Color Copy Section 
 function CopyColor(Button, FromData) {
     Button.addEventListener('click', function () {
-        navigator.clipboard.writeText(FromData.value)
-        ToastMsg.innerHTML=`${FromData.value} is Copied!`
-        ToastMsg.style.color=FromData.value;
+       if(FromData.value==''){
+        navigator.clipboard.writeText('#ffffff')
+        ToastMsg.innerHTML=`${'#ffffff'} is Copied!`
+        
         Toast.style.display='flex';
             setTimeout(()=>{
                 Toast.style.display='none';
             },2000)
+       }else{
+        navigator.clipboard.writeText(FromData.value)
+        ToastMsg.innerHTML=`${FromData.value} is Copied!`
+        
+        Toast.style.display='flex';
+            setTimeout(()=>{
+                Toast.style.display='none';
+            },2000)
+       }
     })
 }
+
+
+// CloseButton Section 
+CloseButton.addEventListener('click',function(){
+    Toast.style.display='none';
+})
 
 // Change Color Function Call 
 
